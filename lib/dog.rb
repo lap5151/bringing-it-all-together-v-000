ec98ac28d4a0710cbe0562f78ff56bcdf3311827
Lauren Pardee <lap5151@gmail.com>
2019-01-16 19:40:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,8 +50,9 @@ attr_accessor :id, :name, :breed
     dog
   end
 
-  def self.find_by_id
-
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    DB[:conn].execute(sql,id)
   end
 
 
