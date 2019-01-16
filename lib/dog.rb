a0fa4e9893bc842f78521846a72e341964bcc532
Lauren Pardee <lap5151@gmail.com>
2019-01-16 19:51:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,7 @@ attr_accessor :id, :name, :breed
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql,id)[0]
     hash = {:id=>result[0], :name=>result[1], :breed=>result[2]}
-    dog = Dog.new(hash)
+    dog = Dog.new({:id=>result[0], :name=>result[1], :breed=>result[2]})
     dog
   end
 
