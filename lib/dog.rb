b2edca0464e2d212c7bd6d2587a3f2a52852edea
Lauren Pardee <lap5151@gmail.com>
2019-01-16 19:15:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,8 +13,16 @@ attr_accessor :id, :name, :breed
     CREATE TABLE IF NOT EXISTS dogs(
       id INTEGER PRIMARY KEY,
       name TEXT,
-      breed TEXT
+      breed TEXT      
     )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql =<<-SQL
+    DROP TABLE dogs
     SQL
 
     DB[:conn].execute(sql)
