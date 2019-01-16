0eba11d54d9b0b6dd0400a4abea032c71cc4e357
Lauren Pardee <lap5151@gmail.com>
2019-01-16 19:24:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,12 @@ attr_accessor :id, :name, :breed
   end
 
   def save
-    
+    sql =<<-SQL
+    INSERT INTO dogs (name,breed)
+    VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql,self.name,self.breed)
   end
 
 
