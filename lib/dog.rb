2ebe22376edb1afe7b0a8170b37854ff31c6efdc
Lauren Pardee <lap5151@gmail.com>
2019-01-16 22:42:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ attr_reader :id
   def self.find_or_create_by(hash)
     result = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", hash[:name],hash[:breed])
     if !result.empty?
-      dog = Dog.new({ :name=>result[0][1], :breed=>result[0][2]})
+      dog = Dog.new({:id=> result[0][0], :name=>result[0][1], :breed=>result[0][2]})
     else
       dog = self.create({:name=>self.name, :breed=>self.breed})
     end
