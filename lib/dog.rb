cb44fa5cae9cb7b7fc31678a91a5fa4cda06dac5
Lauren Pardee <lap5151@gmail.com>
2019-01-16 19:51:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ attr_accessor :id, :name, :breed
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql,id)[0]
-    hash = hash[:id]=>result[0], hash[:name]=>result[1], hash[:breed]=>result[2]
+    hash = {:id=>result[0], :name=>result[1], :breed=>result[2]}
     dog = Dog.new(hash)
     dog
   end
